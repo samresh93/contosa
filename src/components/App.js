029c241f7f61c9b59bff2a11efb13d14aa1d26ce
CodeAstu <codeastu@CodeAstus-MacBook-Air.local>
2022-07-29 11:53:37 +0530

    second commit

---
+++ src/components/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Card from "./Card";
+import Contacts from "../Contacts";
+import Avatar from "./Avatar";
+function App() {
+    return (
+    <div>
+       <h1 className="heading">My Contacts</h1>
+       <Avatar img="https://images.app.goo.gl/zxXCKyShx8Srzqz48"
+       />
+       <Card
+       name={Contacts[0].name}
+       img={Contacts[0].imgURL}
+       tel={Contacts[0].phone}
+       email={Contacts[0].email}
+        />
+
+       <Card
+       name={Contacts[1].name}
+       img={Contacts[1].imgURL}
+       tel={Contacts[1].phone}
+       email={Contacts[1].email}
+        />
+
+       <Card
+       name={Contacts[2].name}
+       img={Contacts[2].imgURL}
+       tel={Contacts[2].phone}
+       email={Contacts[2].email}
+        />
+        </div>
+    );
+}
+
+export default App;
